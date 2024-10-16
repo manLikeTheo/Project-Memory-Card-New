df07ff23198c2e7110733fd46f4b546a6382d53b
theophilus adewale <adewaletheophilus@gmail.com>
2024-10-16 06:03:52 +0100

    Fornt and back images added!

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,21 @@ function App() {
     const fetchCards = async () => {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?limit=6"
+          "https://thronesapi.com/api/v2/Characters"
         );
         const data = await response.json();
-        // console.log(data.results);
-        const pokemonCards = data.results.map((pokemon) => ({
-          name: pokemon.name,
-          image: "https://pokeapi.co/api/v2/pokemon/1/",
+        // console.log(data);
+        const charactersNeeded = data.splice(0, 6);
+        console.log(charactersNeeded);
+        const gotCards = charactersNeeded.map((gotCard) => ({
+          name: gotCard.fullName,
+          image: gotCard.imageUrl,
         }));
-        setCards(pokemonCards);
+        // const pokemonCards = data.results.map((pokemon) => ({
+        //   name: pokemon.name,
+        //   image: "https://pokeapi.co/api/v2/pokemon/1/",
+        // }));
+        setCards(gotCards);
       } catch (error) {
         console.error("Error fetching data:", error.message);
       }
@@ -28,7 +34,7 @@ function App() {
   const shuffleCards = () => {
     const shudffledCards = [...cards, ...cards]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card }));
+      .map((card) => ({ ...card, id: Math.random() }));
     setCards(shudffledCards);
     setTurns(0);
   };
@@ -36,7 +42,7 @@ function App() {
   console.log(cards, turns);
 
   return (
-    <div className="flex flex-col gap-4 justify-center items-center bg-gradient-to-tr from-blue-700 to-purple-900 h-screen">
+    <div className="flex flex-col gap-4 justify-center items-center bg-gradient-to-tr from-blue-700 to-purple-900 ">
       <h1 className="text-4xl text-center font-bold text-white">
         Memory Game!
       </h1>
@@ -46,6 +52,25 @@ function App() {
       >
         New Game
       </button>
+
+      <div className="grid grid-cols-3 gap-4 justify-center items-center border-4 border-purple-300">
+        {cards.map((card) => (
+          <div key={card.name} className="card">
+            <div className="border-4 border-purple-700 cursor-pointer hover:scale-110">
+              <img
+                className="front w-full h-full p-2 rounded-lg"
+                src={card.image}
+                alt={"card-front"}
+              />
+              <img
+                className="back w-full h-full p-2 rounded-lg"
+                src={"/luke-chesser-eICUFSeirc0-unsplash.jpg"}
+                alt="card-back"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
