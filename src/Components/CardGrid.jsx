33754c94875f9bdd23d73badd4bf740ef67c3c69
theophilus adewale <adewaletheophilus@gmail.com>
2024-12-24 05:00:15 +0100

    card grid fitted to screen

--- src/Components/CardGrid.jsx
+++ src/Components/CardGrid.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./Card";
 const CardGrid = ({ cards, onCardClick, firstPick, secondPick }) => {
   return (
-    <div className="grid grid-cols-4 gap-4 shadow-2xl p-5">
+    <div className="grid grid-cols-4 gap-4 shadow-2xl p-5 h-screen">
       {cards.map((card) => (
         <Card
           key={card.id}
